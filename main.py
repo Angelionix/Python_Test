dbf8cc167064228907dae871e33d7210355e2fef
Angelionix <baryshev.max@gmail.com>
2024-07-20 06:56:04 +0300

    Initial
    Создал проект и азовые классы

---
+++ main.py
@@ -0,0 +1,19 @@
+class Book:
+    int id = 0
+    str title = ""
+    str author = ""
+
+    def __init__(self, id, title, author):
+        self.id = id
+        self.title = title
+        self.author = author
+    pass
+
+
+class Library:
+    pass
+
+
+class Main:
+    def __main__(self):
+        pass
