2ba915d67c77f95afa71c1160af25b9f27e41f0a
Angelionix <baryshev.max@gmail.com>
2024-07-22 23:23:46 +0300

    реализованы часть основных функций
    Реализиовано загрузка библиотеки из текстового файла. Реализованы функции: отображения всей библиотеки, добавления и удаления книг

--- Library.py
+++ Library.py
@@ -6,24 +6,24 @@ class Library:
     book_list = []
 
     def __init__(self):
-        pass
-
+        self.load_book_list()
+#dfsfsd
     def add_book(self, title, author, year):
         new_id = -1
         if len(self.free_ids) == 0:
-            new_id = self.book_list[-1].id + 1
+            new_id = int(self.book_list[-1].id) + 1
         else:
             new_id = self.free_ids.pop()
-        new_book = b.Book(new_id, title, author, year, "В наличии")
+        new_book = b.Book(new_id, title, author, year, "In Stock")
         self.book_list.append(new_book)
         respond = [None, dm.OPERATION_SUCCESS]
         return  respond
 
     def delete_book(self, id):
         respond = [None]
-        if -1 < id and id < len(self.book_list):
+        if -1 < id and id <= len(self.book_list):
             for book in self.book_list:
-                if id == book.id:
+                if id == int(book.id):
                     self.book_list.remove(book)
                     respond.append(dm.OPERATION_SUCCESS)
                     break
@@ -45,7 +45,7 @@ class Library:
 
     def show_library(self):
         responce = []
-        if len(self.book_list) >0:
+        if len(self.book_list) > 0:
             responce.append(self.book_list)
             responce.append(dm.OPERATION_SUCCESS)
         else:
@@ -55,7 +55,7 @@ class Library:
 
     def change_status(self, id, new_status):
         responce = [None]
-        if not (new_status == "В наличии" or new_status == "Выдана"):
+        if not (new_status == "In Stock" or new_status == "Issued"):
             responce.append(dm.WRONG_STATUS)
         else:
             if -1 < id and id < len(self.book_list):
@@ -69,7 +69,14 @@ class Library:
         return responce
 
     def load_book_list(self):
-        pass
+        try:
+            with open('book_list.txt', 'r') as file:
+                for line in file:
+                    temp = line.split(sep='/')
+                    new_book = b.Book(temp[0], temp[1], temp[2], temp[3], temp[4][:-1])
+                    self.book_list.append(new_book)
+        except FileNotFoundError:
+            print(dm.FILE_NOT_FOUND)
 
     def save_book_list(self):
         pass
--- main.py
+++ main.py
@@ -3,47 +3,59 @@ import Dialog_messages as dm
 
 
 def show_respond(respond):
-    print(f"ID, Author,  title, year, status")
-    for book in respond:
-        print(f"{book.id} | {book.author} | {book.title} | {book.year} | {book.status}")
+    if None == respond[0]:
+        print(respond[1])
+    else:
+        print(f"{'ID':4}| {'Author':30}| {'Title':50}| {'Year':4}| {'Status':8}")
+        for book in respond[0]:
+            print(f"{book.id:4}| {book.author:30}| {book.title:50}| {book.year:4}| {book.status:8}")
 
-
-is_working = True
-uncorrect_choose = True
-choose = 0
 library = lib.Library()
+is_working = True
+
+
 
 if __name__ == '__main__':
     print(dm.WELCOME_MESSAGE)
     while is_working:
+        uncorrect_choose = True
+        choose = 0
+
+        print()
+        print(dm.CHOOSE_OPTION_MESSAGE)
         print(dm.SHOW_ALL_COMMAND)
         print(dm.ADD_BOOK_COMMAND)
         print(dm.DELETE_BOOK_COMMAND)
         print(dm.SEARCH_BOOK_COMMAND)
         print(dm.CHANGE_BOOK_STATUS_COMAND)
-        print()
-        print(dm.CHOOSE_OPTION_MESSAGE)
+        print(dm.EXIT_PROGRAMM)
+
         while uncorrect_choose:
             try:
                 temp = int(input())
-                if temp > 0 and temp < 6:
+                if temp > 0 and temp <= 6 :
                     choose = temp
                     uncorrect_choose = False
                 else:
                     print(dm.WRONG_CHOOSE_NUMBER_MESSAGE)
             except:
                 print(dm.WRONG_CHOOSE_INPUT_MESSAGE)
+
         match choose:
             case 1:
-                library.show_library()
+                show_respond(library.show_library())
             case 2:
-                pass
+                new_title = input("Введите название книги: ")
+                new_author = input("Введите имя автора: ")
+                new_year = input("Введите год издания: ")
+                show_respond(library.add_book(new_title, new_author, new_year))
             case 3:
-                pass
+                id_to_del = int(input("Введите id книги которую выхотите удалить из базы: "))
+                show_respond(library.delete_book(id_to_del))
             case 4:
+
                 pass
             case 5:
                 pass
-        s = input()
-        if s == " ":
-          is_working = False
+            case 6:
+                is_working = False
